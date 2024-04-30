14456c7beda669fdf2b03eeee23172d820c0e300
27979831 <27979831@qq.com>
2024-04-30 20:58:24 +0800

    修复添加API时，模拟盘和实盘只能选一个的bug

--- api/views/accountApi.py
+++ api/views/accountApi.py
@@ -70,7 +70,7 @@ class ApiAddView(CopyCreateModelMixin, CopyListModelMixin, CopyDestroyModelMixin
                         'error': '当前API无法进行合约交易，请在交易所合约交易页面手动设置！'})
                 # uid
                 uid = res.get('data')[0].get('uid')
-                if models.ApiInfo.objects.filter(uid=uid, deleted=False).exists():
+                if models.ApiInfo.objects.filter(uid=uid, flag=flag, deleted=False).exists():
                     return Response({
                         'code': return_code.EXIST_ERROR,
                         'error': '当前交易所账户已授权，请勿重复绑定！可在交易所申请子账户继续绑定！'})
